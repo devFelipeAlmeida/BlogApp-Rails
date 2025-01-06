8a5849d2383acd086677b9ddcce75418854f6ffd
devFelipeAlmeida <felipelongo321@gmail.com>
2025-01-06 03:13:58 -0300

    organization importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.3.0/dist/js/bootstrap.esm.js"
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "@popperjs--core.js"
