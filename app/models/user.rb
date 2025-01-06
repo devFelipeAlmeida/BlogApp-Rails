cefc79bda87a42ca81127bb7c7985c6ebcd7a21b
devFelipeAlmeida <felipelongo321@gmail.com>
2025-01-06 14:06:42 -0300

    organization model/post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,9 @@
 class User < ApplicationRecord
-  has_many_attached :files
-
-  
   has_many :posts, dependent: :destroy
 
   validates :name, presence: true
   validates :email, uniqueness: { case_sensitive: false }
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
