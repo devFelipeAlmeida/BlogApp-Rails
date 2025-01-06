5654ff2db34ac135457e8993c2bb97f560449385
devFelipeAlmeida <felipelongo321@gmail.com>
2025-01-06 04:40:22 -0300

    fix problem upload

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,21 +1,31 @@
 class UploadsController < ApplicationController
   def process_upload
     file = params[:file]
-  
+
     if file && file.content_type == "text/plain"
-      unique_filename = "#{Time.now.to_i}_#{file.original_filename}"
-      blob = ActiveStorage::Blob.create_and_upload!(
-        io: file.tempfile, 
-        filename: unique_filename, 
-        content_type: file.content_type
-      )
-  
-      UploadProcessorJob.perform_later(blob.id, current_user.id)
-  
-      redirect_to root_path, notice: t("flash.posts.upload_sidekiq")
+      if valid_txt_format?(file)
+        unique_filename = "#{Time.now.to_i}_#{file.original_filename}"
+        blob = ActiveStorage::Blob.create_and_upload!(
+          io: file.tempfile, 
+          filename: unique_filename, 
+          content_type: file.content_type
+        )
+
+        UploadProcessorJob.perform_later(blob.id, current_user.id)
+        
+        redirect_to root_path, notice: t("flash.posts.upload_sidekiq")
+      else
+        redirect_to root_path, alert: t('flash.posts.wrong_file')
+      end
     else
-      redirect_to root_path, alert: t("flash.posts.not_upload_sidekiq")
+      redirect_to root_path, alert: t('flash.posts.not_upload_sidekiq')
     end
   end
-  
+
+  private
+
+  def valid_txt_format?(file)
+    file_data = file.read
+    file_data.split("\n").all? { |line| line.strip.split("|").size >= 2 }
+  end
 end
--- app/jobs/upload_processor_job.rb
+++ app/jobs/upload_processor_job.rb
@@ -3,26 +3,20 @@ class UploadProcessorJob < ApplicationJob
 
   def perform(blob_id, user_id)
     blob = ActiveStorage::Blob.find_by(id: blob_id)
-    return if blob.nil?
+    return unless blob
 
-    begin
-      file_data = blob.download
-    rescue => e
-      return
-    end
+    file_data = blob.download
+    return unless file_data
 
     tempfile = Tempfile.new(['uploaded_file', '.txt'])
     tempfile.binmode
     tempfile.write(file_data)
     tempfile.rewind
 
-    begin
-      process_file(tempfile.path, user_id)
-    rescue => e
-    ensure
-      tempfile.close
-      tempfile.unlink
-    end
+    process_file(tempfile.path, user_id)
+
+    tempfile.close
+    tempfile.unlink
   end
 
   private
@@ -31,6 +25,8 @@ class UploadProcessorJob < ApplicationJob
     user = User.find(user_id)
 
     File.readlines(file_path).each do |line|
+      next unless line.include?("|")
+
       title, content, tags = line.strip.split("|")
       next unless title.present? && content.present?
 
@@ -43,8 +39,6 @@ class UploadProcessorJob < ApplicationJob
           Tag.create!(name: tag_name, post_id: post.id)
         end
       end
-    rescue ActiveRecord::RecordInvalid => e
-      Rails.logger.error "Erro ao processar linha: #{line.strip}. Detalhes: #{e.message}"
     end
   end
 end
