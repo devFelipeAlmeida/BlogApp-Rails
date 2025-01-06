ff312523cc6f88bf7b9f5dc12958d0720a70ef6e
devFelipeAlmeida <felipelongo321@gmail.com>
2025-01-06 05:57:47 -0300

    rspec request uploads

--- spec/requests/posts_uploads_spec.rb
+++ spec/requests/posts_uploads_spec.rb
@@ -1,40 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
+RSpec.describe "UploadsController", type: :request do
+  let(:user) { create(:user) }  # Criando um usuário para testar a ação com autenticação, se necessário
+  let(:file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'sample.txt'), 'text/plain') }  # Arquivo de teste
+  
   before do
-    ActiveJob::Base.queue_adapter = :test
+    sign_in user  # Se estiver usando Devise para autenticação
   end
 
-  let(:user) { create(:user) } # Crie um usuário de teste
-  let(:file) { fixture_file_upload('sample.txt', 'text/plain') } # Arquivo fictício de texto
-  let(:invalid_file) { fixture_file_upload('invalid_file.jpg', 'image/jpeg') }
-
-  before do
-    sign_in user
-  end
-
-  describe 'POST #process_upload' do
-    context 'quando o arquivo é válido' do
-      it 'enfileira o job de processamento e cria posts' do
-        expect(Post.count).to eq(0)
-
+  describe "POST /uploads/process_upload" do
+    context "quando o arquivo é válido" do
+      it "faz o upload do arquivo e processa o job" do
         expect {
           post process_upload_path, params: { file: file }
-        }.to have_enqueued_job(UploadProcessorJob).with(
-          an_instance_of(String), user.id
-        )
+        }.to have_enqueued_job(UploadProcessorJob)  # Verifica se o job foi enfileirado
 
-        expect(response).to redirect_to(root_path)
-        expect(flash[:notice]).to eq(I18n.t("flash.posts.upload_sidekiq"))
+        expect(response).to redirect_to(root_path)  # Verifica se a resposta é um redirecionamento
+        follow_redirect!
+
+        expect(flash[:notice]).to eq(I18n.t("flash.posts.upload_sidekiq"))  # Verifica a mensagem de sucesso
       end
     end
 
-    context 'quando o arquivo é inválido' do
-      it 'não enfileira o job e exibe uma mensagem de erro' do
-        post process_upload_path, params: { file: invalid_file }
+    context "quando o arquivo não é válido" do
+      let(:invalid_file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'invalid_file.jpg'), 'application/jpg') }
 
-        expect(response).to redirect_to(root_path)
-        expect(flash[:alert]).to eq(I18n.t("flash.posts.not_upload_sidekiq"))
+      it "não faz o upload e exibe uma mensagem de erro" do
+        post process_upload_path, params: { file: invalid_file }
+        
+        expect(response).to redirect_to(root_path)  # Verifica o redirecionamento
+        follow_redirect!
+
+        expect(flash[:alert]).to eq(I18n.t("flash.posts.not_upload_sidekiq"))  # Verifica a mensagem de erro
       end
     end
   end
